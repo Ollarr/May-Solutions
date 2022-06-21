4cb349de0be679f3c22ff9afdcf793b1c0d9b440
Oladimeji <oluyedis@gmail.com>
2022-06-21 18:22:16 +0100

    created navbar UI

---
+++ index.js
@@ -0,0 +1,13 @@
+// Header section
+function classToggle() {
+    const navs = document.querySelectorAll('.navbar-items')
+    
+    navs.forEach(nav => nav.classList.toggle('.navbar-toggle-show'));
+  }
+  
+  document.querySelector('.navbar-link-toggle').addEventListener('click', classToggle);
+
+
+
+// Order section
+
