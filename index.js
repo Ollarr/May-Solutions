fe60a769b5350f5e70026f82b13fccb387e3d541
Oladimeji <oluyedis@gmail.com>
2022-06-22 14:23:38 +0100

    hero section

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 // Header section
-function classToggle() {
-    const navs = document.querySelectorAll('.navbar-items')
-    
-    navs.forEach(nav => nav.classList.toggle('.navbar-toggle-show'));
-  }
-  
-  document.querySelector('.navbar-link-toggle').addEventListener('click', classToggle);
-
-
 
 // Order section
+var swiper = new Swiper(".menu-slider", {
+    grabCursor:true,
+    loop:true,
+    autoHeight:true,
+    centeredSlides:true,
+    spaceBetween: 20,
+    pagination: {
+       el: ".swiper-pagination",
+       clickable: true,
+    },
+    breakpoints: {
+       0: {
+         slidesPerView: 1,
+       },
+       700: {
+         slidesPerView: 1,
+       },
+       1000: {
+         slidesPerView: 1,
+       },
+    },
+    
+ });
 
+ var swiper = new Swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+ });
+ 
